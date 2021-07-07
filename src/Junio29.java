e5afd51a9157fe42a5c0ec7106ee0728e5306a22
edwgk <edwg1782@gmail.com>
2021-07-07 12:13:20 -0500

    ejercicio u1

--- src/Junio29.java
+++ src/Junio29.java
@@ -3,20 +3,20 @@ import java.util.Scanner;
 public class Junio29 {
 
     public static void main(String[] args){
-        //Scanner scanner=new Scanner(System.in);
+        Scanner scanner=new Scanner(System.in);
         //variable=input()
-        //System.out.println("Ingresa una palabra:");
-        //String entrada=scanner.nextLine();
-        //System.out.println("El mensaje fue "+entrada);
+        System.out.println("Ingresa una palabra:");
+        String entrada=scanner.nextLine();
+        System.out.println("El mensaje fue "+entrada);
         //int resultado=scanner.nextInt();
         //System.out.println("El mensaje fue "+resultado);
-        if(5>7){
-            System.out.println(":)");
-        }else if(3>2){
-            System.out.println("T.T");
-        }else{
-            System.out.println(":(");
-        }
+        //if(5>7){
+        //    System.out.println(":)");
+        //}else if(3>2){
+        //    System.out.println("T.T");
+        //}else{
+        //    System.out.println(":(");
+        //}
         
 
     }
--- src/unit1_ejercicios01.java
+++ src/unit1_ejercicios01.java
@@ -2,20 +2,47 @@ import java.util.Scanner;
 
 public class unit1_ejercicios01 {
     public static void main(String[] args){
-
+        
         Scanner numero=new Scanner(System.in);
         System.out.println("Escriba primer numero:");
         float numero1=numero.nextFloat();
-        
         System.out.println("Escriba segundo numero:");
         float numero2=numero.nextFloat();
-        
-        float resultado=numero1+numero2;
-        String operacion="Suma";
-        System.out.println("La "+operacion+" de "+numero1+" y "+numero2+" es igual a "+resultado);
 
+        float resultado=sumar(numero1,numero2);
+        float resultado2=restar(numero1,numero2);
+        float resultado3=multiplicar(numero1,numero2);
+        
         
         
         
     }
+
+    public static float sumar(float numero1,float numero2){
+        float resultado=numero1+numero2;
+        String operacion="Suma";
+        System.out.println("La "+operacion+" de "+numero1+" y "+numero2+" es igual a "+resultado);
+
+        return resultado;
+    }
+
+    public static float restar(float numero1,float numero2){
+        float resultado2=numero1-numero2;
+        String operacion="Resta";
+        System.out.println("La "+operacion+" de "+numero1+" y "+numero2+" es igual a "+resultado2);
+
+        return resultado2;
+    }
+    public static float multiplicar(float numero1,float numero2){
+        float resultado3=numero1*numero2;
+        String operacion="Multplicacion";
+        System.out.println("La "+operacion+" de "+numero1+" y "+numero2+" es igual a "+resultado3);
+
+        return resultado3;
+    }
+    
+        
+
+        
+        
 }
